9708cbc13311a615915905feaaf965203ac4b14c
Sean <571175497@qq.com>
2023-03-10 11:56:27 +0800

    增加了一行没用的注释

--- Test/lmj/exp1/pro.v
+++ Test/lmj/exp1/pro.v
@@ -4,6 +4,7 @@ module priority_encoder(Data,GS,Code,LED);
     output GS;    
     output [4:0] LED;
     //EG4S20 IO默认弱上拉，因此需要输出低电平熄灭LED7-3
+    //...
     assign LED = 5'b0;
     assign GS = Data ? 1'b1 : 1'b0;
     always@(Data)
