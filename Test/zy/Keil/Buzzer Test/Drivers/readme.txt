c4bc6c56b5a50b04301ddd562cb1c059504384ce
YanZhang <870543028@qq.com>
2023-03-16 23:17:22 +0800

    更新了跑马灯实验和蜂鸣器实验的keil文件和hex等文件

---
+++ Test/zy/Keil/Buzzer Test/Drivers/readme.txt
@@ -0,0 +1,21 @@
+    该文件夹下面存放驱动层代码，包括：
+    1，BSP：正点原子提供的板级支持包驱动代码（原HARDWARE文件夹下的代码），如：LED、BEEP、KEY、EXTI、TIMER、WDG...等
+    2，CMSIS：ARM提供的CMSIS代码（主要包括各种头文件和启动文件（.s文件），为了方便使用，减少占用空间，我们进行了精简）
+    3，STM32F1xx_HAL_Driver：ST提供的F1xx HAL库驱动代码
+ 
+ 
+
+ ***********************************************************************************************************
+ * 公司名称：广州市星翼电子科技有限公司（正点原子）
+ * 电话号码：020-38271790
+ * 传真号码：020-36773971
+ * 公司网址：www.alientek.com
+ * 购买地址：zhengdianyuanzi.tmall.com
+ * 技术论坛：http://www.openedv.com/forum.php
+ * 最新资料：www.openedv.com/docs/index.html
+ *
+ * 在线视频：www.yuanzige.com
+ * B 站视频：space.bilibili.com/394620890
+ * 公 众 号：mp.weixin.qq.com/s/y--mG3qQT8gop0VRuER9bw
+ * 抖    音：douyin.com/user/MS4wLjABAAAAi5E95JUBpqsW5kgMEaagtIITIl15hAJvMO8vQMV1tT6PEsw-V5HbkNLlLMkFf1Bd
+ ***********************************************************************************************************
---
+++ Test/zy/Keil/Marquee Lamp Experiment/User/main.c
@@ -0,0 +1,44 @@
+/**
+ ****************************************************************************************************
+ * @file        main.c
+ * @author      正点原子团队(ALIENTEK)
+ * @version     V1.0
+ * @date        2020-04-20
+ * @brief       跑马灯 实验
+ * @license     Copyright (c) 2020-2032, 广州市星翼电子科技有限公司
+ ****************************************************************************************************
+ * @attention
+ *
+ * 实验平台:正点原子 STM32F103开发板
+ * 在线视频:www.yuanzige.com
+ * 技术论坛:www.openedv.com
+ * 公司网址:www.alientek.com
+ * 购买地址:openedv.taobao.com
+ *
+ ****************************************************************************************************
+ */
+
+#include "./SYSTEM/sys/sys.h"
+#include "./SYSTEM/delay/delay.h"
+#include "./SYSTEM/usart/usart.h"
+#include "./BSP/LED/led.h"
+
+
+int main(void)
+{
+    HAL_Init();                                 /* 初始化HAL库 */
+    sys_stm32_clock_init(RCC_PLL_MUL9);         /* 设置时钟,72M */
+    delay_init(72);                             /* 初始化延时函数 */
+    led_init();                                 /* 初始化LED */
+    
+    while(1)
+    {
+        LED0(0);                                /* LED0 亮 */
+        LED1(1);                                /* LED1 灭 */
+        delay_ms(500);
+        LED0(1);                                /* LED0 灭 */
+        LED1(0);                                /* LED1 亮 */
+        delay_ms(500);
+    }
+}
+
