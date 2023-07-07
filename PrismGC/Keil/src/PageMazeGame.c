6e64a820ca90ff5612fedc3057379e3d41f0d48c
YanZhang <870543028@qq.com>
2023-07-07 14:31:36 +0800

    画出起点、终点和陷阱方块

--- PrismGC/Keil/src/PageMazeGame.c
+++ PrismGC/Keil/src/PageMazeGame.c
@@ -11,6 +11,7 @@
 uint8_t PageMazeGame()
 {
     uint32_t nowTime;
+    uint32_t pixels[8]={RED,RED,RED,GREEN,GREEN,GREEN,BLUE,BLUE};
 
     while(1)
     {
@@ -21,7 +22,10 @@ uint8_t PageMazeGame()
 
         PingPong();
         LCDBackground(0x888888);
-        
+
+        for(int i=0;i<30;i++)
+            LCDPixels(pixels,30,30+i,8);
+
         uint32_t xInit=(H_DISP/2)-(MAP_W-1)*BLOCK_SIZE+120;
         uint32_t yInit=(V_DISP/2)-(MAP_H-1)*BLOCK_SIZE;
         for(uint32_t i=0;i<MAP_W;i++)
@@ -30,11 +34,18 @@ uint8_t PageMazeGame()
             {
                 uint32_t x=xInit+2*j*BLOCK_SIZE;
                 uint32_t y=yInit+2*i*BLOCK_SIZE;
-                switch (level1[i][j])
+                switch (level1.map[i][j])
                 {
                     case B_ICE: BlockICE(x,y); break;
                     case B_BAR: BlockBAR(x,y); break;
+                    case B_END: BlockEnd(x,y); break;
+                    case B_TRAP: BlockTrap(x,y); break;
                 }
+                if(i==level1.istart&&j==level1.jstart)
+                {
+                    BlockOrigin(x,y);
+                }
+                
             }
         }
 
