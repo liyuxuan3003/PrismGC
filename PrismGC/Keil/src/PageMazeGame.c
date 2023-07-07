ba1a00b8715ae56454c561f94821d4adb2eddf03
liyuxuan <liyuxuan3003@outlook.com>
2023-07-07 21:29:03 +0800

    优化了上下左右的移动

--- PrismGC/Keil/src/PageMazeGame.c
+++ PrismGC/Keil/src/PageMazeGame.c
@@ -13,30 +13,37 @@ static uint8_t CharPlace()
 {
     switch (KEYBOARD->KEY)
     {
-        case 0x09: return PMG_R; break;
-        case 0x05: return PMG_L; break;
+        case 0x05: return PMG_R; break;
+        case 0x07: return PMG_L; break;
         case 0x0A: return PMG_U; break;
-        case 0x06: return PMG_D; break;
+        case 0x02: return PMG_D; break;
         default: return 0; break;
     }
 }
 
+static uint32_t CalX(uint32_t i,uint32_t j)
+{
+    return X_CORNER+2*j*BLOCK_SIZE;
+}
+
+static uint32_t CalY(uint32_t i,uint32_t j)
+{
+    return Y_CORNER+2*i*BLOCK_SIZE;
+}
 
 uint8_t PageMazeGame()
 {
     uint32_t nowTime;
-    uint32_t xCorner=(H_DISP/2)-(MAP_W-1)*BLOCK_SIZE+120;
-    uint32_t yCorner=(V_DISP/2)-(MAP_H-1)*BLOCK_SIZE;
-    uint32_t iCharacterMove=level1.istart;
-    uint32_t jCharacterMove=level1.jstart;
-    uint32_t xCharMove=iCharacterMove;
-    uint32_t yCharMove=jCharacterMove;
+
+    uint32_t chmI=level1.istart;
+    uint32_t chmJ=level1.jstart;
     while(1)
     {
         nowTime = TIMER -> TIME;
 
         if(KEYBOARD -> KEY == 0xF)
             return PAGE_MAIN;
+
 
         PingPong();
         LCDBackground(0x888888);
@@ -45,37 +52,49 @@ uint8_t PageMazeGame()
         {
             for(uint32_t j=0;j<MAP_H;j++)
             {
-
-                uint32_t x=xCorner+2*j*BLOCK_SIZE;
-                uint32_t y=yCorner+2*i*BLOCK_SIZE;
+                uint32_t x=CalX(i,j);
+                uint32_t y=CalY(i,j);
                 switch (level1.map[i][j])
                 {
                     case B_ICE: BlockICE(x,y); break;
                     case B_BAR: BlockBAR(x,y); break;
                     case B_END: BlockEND(x,y); break;
                     case B_TRP: BlockTRP(x,y); break;
-                };
-                
-            }
-        };
-        switch (level1.map[iCharacterMove][jCharacterMove])
-        {
-            case B_ICE:
-            {
-                switch(CharPlace())
-                {
-                    case PMG_L: jCharacterMove--; break;
-                    case PMG_R: jCharacterMove++; break;
-                    case PMG_U: iCharacterMove--; break;
-                    case PMG_D: iCharacterMove++; break;
-                    case 0: break;
                 }
             }
-            case B_BAR: break;
         }
-        xCharMove=xCorner+2*jCharacterMove*BLOCK_SIZE;
-        yCharMove=yCorner+2*iCharacterMove*BLOCK_SIZE;
-        MainCharactor (xCharMove,yCharMove);
+
+        uint32_t chmX=CalX(chmI,chmJ);
+        uint32_t chmY=CalY(chmI,chmJ);
+        MainCharactor (chmX,chmY);
+
+        uint32_t chmNextI=chmI;
+        uint32_t chmNextJ=chmJ;
+        uint8_t isMove=1;
+        switch(CharPlace())
+        {
+            case PMG_L: chmNextJ--; break;
+            case PMG_R: chmNextJ++; break;
+            case PMG_U: chmNextI--; break;
+            case PMG_D: chmNextI++; break;
+            case 0: isMove=0; break;
+        }
+        
+        if(isMove)
+        {
+            switch (level1.map[chmNextI][chmNextJ])
+            {
+                case B_ICE:
+                {
+                    chmI=chmNextI;
+                    chmJ=chmNextJ;
+                    break;
+                }
+                case B_BAR: break;
+                default:    break;
+            }
+        }
+
         while(TIMER -> TIME < nowTime + FRAME);
     }
 }
--- PrismGC/Keil/src/PageMazeGame.h
+++ PrismGC/Keil/src/PageMazeGame.h
@@ -6,7 +6,14 @@
 #define PMG_U 3
 #define PMG_D 4
 
+#include "GPULite.h"
+#include "Block.h"
+#include "BlockMap.h"
+
 #include <stdint.h>
+
+#define X_CORNER (H_DISP/2)-(MAP_W-1)*BLOCK_SIZE+120
+#define Y_CORNER (V_DISP/2)-(MAP_H-1)*BLOCK_SIZE
 
 uint8_t PageMazeGame();
 
