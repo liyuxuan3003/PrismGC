3397047af08b1c8120a73223a232663502df0a4b
liyuxuan <liyuxuan3003@outlook.com>
2023-07-10 13:13:16 +0800

    修正了菜单界面的问题，将菜单和游戏连接起来

--- PrismGC/Keil/src/PageMenu.h
+++ PrismGC/Keil/src/PageMenu.h
@@ -5,9 +5,6 @@
 
 #define PAGEMAX 2
 
-void LevelMenu (uint32_t x,uint32_t y,int type,int NUM);
-void BlockMenu (uint32_t x,uint32_t y,int type);
-void HighLight (uint32_t x,uint32_t y);
 uint8_t PageMenu();
 
 
