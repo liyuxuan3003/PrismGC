f64f40ed9f8ddcf46687105dacbc0bd5a201b296
liyuxuan <liyuxuan3003@outlook.com>
2023-07-11 12:58:19 +0800

    暂时改回用按键控制

--- PrismGC/Keil/src/PageMazeGame.c
+++ PrismGC/Keil/src/PageMazeGame.c
@@ -13,22 +13,22 @@
 
 static uint8_t GetMoveDirection()
 {
-    // switch(KEYBOARD->KEY)
-    // {
-    //     case 0x05: return PMG_R; break;
-    //     case 0x07: return PMG_L; break;
-    //     case 0x0A: return PMG_U; break;
-    //     case 0x02: return PMG_D; break;
-    //     default: return 0; break;
-    // }
-    switch(NunchuckKey())
+    switch(KEYBOARD->KEY)
     {
-        case 'R': return PMG_R; break;
-        case 'L': return PMG_L; break;
-        case 'U': return PMG_U; break;
-        case 'D': return PMG_D; break;
+        case 0x05: return PMG_R; break;
+        case 0x07: return PMG_L; break;
+        case 0x0A: return PMG_U; break;
+        case 0x02: return PMG_D; break;
         default: return 0; break;
     }
+    // switch(NunchuckKey())
+    // {
+    //     case 'R': return PMG_R; break;
+    //     case 'L': return PMG_L; break;
+    //     case 'U': return PMG_U; break;
+    //     case 'D': return PMG_D; break;
+    //     default: return 0; break;
+    // }
 }
 
 static int32_t CalX(MapCoord coord)
